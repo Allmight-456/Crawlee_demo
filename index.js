0a382784167566618153a7b08ca227a64a7808f6
Ishan Kumar <ishankumar@Ishans-MacBook-Air.local>
2024-09-24 21:55:03 +0530

    checking out the automation in website crawling visually

---
+++ index.js
@@ -0,0 +1,28 @@
+// delete stoarge after npm run dev if want to rerun the same website 
+//Do a control+c if doing a headfull crawling to exit chromium browser
+import { PlaywrightCrawler, Dataset } from 'crawlee';
+
+// PlaywrightCrawler crawls the web using a headless
+// browser controlled by the Playwright library.
+const crawler = new PlaywrightCrawler({
+    // Use the requestHandler to process each of the crawled pages.
+    async requestHandler({ request, page, enqueueLinks, log }) {
+        const title = await page.title();
+        log.info(`Title of ${request.loadedUrl} is '${title}'`);
+
+        // Save results as JSON to ./storage/datasets/default
+        await Dataset.pushData({ title, url: request.loadedUrl });
+
+        // Extract links from the current page
+        // and add them to the crawling queue.
+        await enqueueLinks();
+    },
+    // Uncomment this option to see the browser window.
+    headless: false,
+
+    // Let's limit our crawls to make our tests shorter and safer.
+    maxRequestsPerCrawl: 50,
+});
+
+// Add first URL to the queue and start the crawl.
+await crawler.run(['https://www.youtube.com']);
